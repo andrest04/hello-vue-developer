fbe37f90e865eaeacf82d132f6a22e1b2e171483
Andres Torres <andresalbertotorresgarcia@gmail.com>
2024-08-31 12:00:40 -0500

    feat(welcoming): added register developer component

---
+++ src/welcoming/components/register-developer.component.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "register-developer", // convencion de nombre de componente
+  data() { // datos del formulario
+    return {
+      firstName: '',
+      lastName: '',
+    }
+  },
+  methods: { // metodo para registrar un desarrollador
+    onSubmit(){
+      console.log("Registering developer"); // log para verificar que se esta ejecutando la funcion
+      this.$emit('developer-registered', {firstName: this.firstName, lastName: this.lastName}); // emitir evento con los datos del formulario
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
