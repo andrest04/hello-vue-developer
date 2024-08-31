a71c97e3e5e31a98f75ed09e49d9154199c14470
Andres Torres <andresalbertotorresgarcia@gmail.com>
2024-08-31 12:16:06 -0500

    feat(welcoming): added welcome component

---
+++ src/welcoming/components/welcome.component.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "welcome", // por que queremos que el selector sea el nombre
+  props: {
+    firstName: String,
+    lastName: String
+  },
+  methods: {
+    developerName() {
+      let developer = new Developer(this.firstName, this.lastName);
+      return developer.fullName;
+    }
+  }
+}
+</script>
+
+<template>
+  <p> Hello {{developerName()}}, Now you are a Vue Developer </p> <!-- se puede llamar la funcion derectamente en el parrafo-->
+</template>
+
+<style scoped>
+
+</style>
